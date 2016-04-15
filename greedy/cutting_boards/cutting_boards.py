6083f0f112728aaf54a44947d7eaefe29d1d3501
Threynaud <thmsreynaud@gmail.com>
2016-04-16 00:24:24 +0200

    cutting boards

--- greedy/cutting_boards/cutting_boards.py
+++ greedy/cutting_boards/cutting_boards.py
@@ -3,13 +3,31 @@
 import sys
 
 
-def min_cost(m, n, Y, X):
-    h_cuts = 0  # Number of cuts along Y
-    v_cuts = 0  # Number of cuts along X
+def min_cost(Y, X):
+    x_cuts = 1  # Number of segments along Y
+    y_cuts = 1  # Number of segments along X
     total = 0  # Total cost
-    X.sort()
-    Y.sort()
+    X.sort(reverse=True)
+    Y.sort(reverse=True)
 
+    while len(Y) > 0 and len(X) > 0:
+        if X[0] >= Y[0]:
+            x_cuts += 1
+            total += X[0] * y_cuts
+            X.pop(0)
+        else:
+            y_cuts += 1
+            total += Y[0] * x_cuts
+            Y.pop(0)
+
+    while len(X) > 0:
+        total += X[0] * y_cuts
+        X.pop(0)
+
+    while len(Y) > 0:
+        total += Y[0] * x_cuts
+        Y.pop(0)
+    return total % (10**9 + 7)
 
 if __name__ == "__main__":
     t = int(sys.stdin.readline().strip())
@@ -17,4 +35,4 @@ if __name__ == "__main__":
         m, n = list(map(int, sys.stdin.readline().split()))
         Y = list(map(int, sys.stdin.readline().split()))
         X = list(map(int, sys.stdin.readline().split()))
-        print(min_cost(m, n, Y, X))
+        print(min_cost(Y, X))
